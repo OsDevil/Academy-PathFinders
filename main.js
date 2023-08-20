efd1d9407230612440519c869406ed5da46687f0
OsDevil <142634035+OsDevil@users.noreply.github.com>
2023-08-20 23:19:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+/* INCLUSION HEADER + FOOTER */
+function includeHeader() {
+    document.body.classList.add('loading');
+    const headerPlaceholder = document.getElementById("header-placeholder");
+    const footerPlaceholder = document.getElementById("footer-placeholder");
+    fetch("header-base.html")
+        .then(response => response.text())
+        .then(content => {
+            headerPlaceholder.innerHTML = content;
+            document.body.classList.remove('loading');
+        });
+    fetch("footer-base.html")
+        .then(response => response.text())
+        .then(content => {
+            footerPlaceholder.innerHTML = content;
+        });
+}
+
+document.addEventListener("DOMContentLoaded", includeHeader);
+
 const menuBtn = document.querySelector(".menu-btn");
 const menuItems = document.querySelector(".menu-items");
 const expandBtn = document.querySelectorAll(".expand-btn");
