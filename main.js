0ceda92280637fbc099d8564b8ab5597c544108c
OsDevil <142634035+OsDevil@users.noreply.github.com>
2023-08-21 15:55:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,70 @@
 document.addEventListener("DOMContentLoaded", () => {
+// Fonctions de gestion des blocs
+function toggleBlock(block, contentBlock, otherBlock, otherContentBlock, introBlock) {
+  if (block.classList.contains("activeBloc")) {
+    block.classList.remove("activeBloc");
+    contentBlock.classList.remove("activeContentBloc");
+
+    if (!otherBlock.classList.contains("activeBloc")) {
+      introBlock.classList.remove("activeIntro");
+    }
+  } else {
+    block.classList.add("activeBloc");
+    contentBlock.classList.add("activeContentBloc");
+
+    if (!otherBlock.classList.contains("activeBloc")) {
+      introBlock.classList.add("activeIntro");
+    }
+
+    if (otherBlock.classList.contains("activeBloc")) {
+      otherBlock.classList.remove("activeBloc");
+      otherContentBlock.classList.remove("activeContentBloc");
+    }
+  }
+}
+
+// Fonctions d'écoute d'événement
+function setupBlocks() {
+  const histoireBloc = document.querySelector(".histoireBloc");
+  const ressourcesBloc = document.querySelector(".ressourcesBloc");
+  const contentHistoire = document.querySelector(".contentHistoire");
+  const contentRessources = document.querySelector(".contentRessources");
+  const contentIntro = document.querySelector(".contentIntro");
+
+  histoireBloc.addEventListener("click", function () {
+    toggleBlock(histoireBloc, contentHistoire, ressourcesBloc, contentRessources, contentIntro);
+  });
+
+  ressourcesBloc.addEventListener("click", function () {
+    toggleBlock(ressourcesBloc, contentRessources, histoireBloc, contentHistoire, contentIntro);
+  });
+}
+
+// Initialisation
+(function() {
+  setupBlocks();
+})();
+
+// Gestion du menu
+(function() {
   const menuBtn = document.querySelector(".menu-btn");
   const menuItems = document.querySelector(".menu-items");
   const expandBtn = document.querySelectorAll(".expand-btn");
 
-  // humburger toggle
+  // Hamburger toggle
   menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("open");
     menuItems.classList.toggle("open");
   });
 
-  // mobile menu expand
+  // Mobile menu expand
   expandBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
       btn.classList.toggle("open");
     });
   });
+})();
+
 });
 
 
