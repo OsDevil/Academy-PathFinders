96cceb5d5973820e6e4feec40af65fc01761c4db
OsDevil <142634035+OsDevil@users.noreply.github.com>
2023-08-21 16:54:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -19,26 +19,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Fonctions de gestion des blocs
     function toggleBlock(block, contentBlock, otherBlock, otherContentBlock, introBlock) {
-         if (block.classList.contains("activeBloc")) {
+        if (block.classList.contains("activeBloc")) {
             block.classList.remove("activeBloc");
             contentBlock.classList.remove("activeContentBloc");
         
             if (!otherBlock.classList.contains("activeBloc")) {
-              introBlock.classList.remove("activeIntro");
+                introBlock.classList.remove("activeIntro");
             }
-          } else {
+        } else {
             block.classList.add("activeBloc");
             contentBlock.classList.add("activeContentBloc");
         
             if (!otherBlock.classList.contains("activeBloc")) {
-              introBlock.classList.add("activeIntro");
+                introBlock.classList.add("activeIntro");
             }
         
             if (otherBlock.classList.contains("activeBloc")) {
-              otherBlock.classList.remove("activeBloc");
-              otherContentBlock.classList.remove("activeContentBloc");
+                otherBlock.classList.remove("activeBloc");
+                otherContentBlock.classList.remove("activeContentBloc");
             }
-          }
+        }
     }
 
     // Fonctions d'écoute d'événement
@@ -58,32 +58,27 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-// Initialisation
-(function() {
-  setupBlocks();
-})();
+    // Initialisation des blocs
+    setupBlocks();
 
-/* GESTION SCROLL HEADER */
+    // Gestion du scroll de l'en-tête
+    let lastScrollPosition = 0;
 
-let lastScrollPosition = 0;
+    function handleScroll() {
+        const currentScrollPosition = window.scrollY;
 
-function handleScroll() {
-    const currentScrollPosition = window.scrollY;
+        if (currentScrollPosition > lastScrollPosition) {
+            document.querySelector('header').classList.add('hidden');
+        } else {
+            document.querySelector('header').classList.remove('hidden');
+        }
 
-    if (currentScrollPosition > lastScrollPosition) {
-        // Scrolling vers le bas, masquer l'en-tête
-        document.querySelector('header').classList.add('hidden');
-    } else {
-        // Scrolling vers le haut, afficher l'en-tête
-        document.querySelector('header').classList.remove('hidden');
+        if (currentScrollPosition === 0) {
+            document.querySelector('header').classList.remove('hidden');
+        }
+
+        lastScrollPosition = currentScrollPosition;
     }
 
-    // Ajouter une condition pour afficher l'en-tête en haut de la page
-    if (currentScrollPosition === 0) {
-        document.querySelector('header').classList.remove('hidden');
-    }
-
-    lastScrollPosition = currentScrollPosition;
-}
     window.addEventListener('scroll', handleScroll);
 });
