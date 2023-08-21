12275b214cf30879fd5003773b6d7881b951b33c
OsDevil <142634035+OsDevil@users.noreply.github.com>
2023-08-21 16:50:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,73 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-// Fonctions de gestion des blocs
-function toggleBlock(block, contentBlock, otherBlock, otherContentBlock, introBlock) {
-  if (block.classList.contains("activeBloc")) {
-    block.classList.remove("activeBloc");
-    contentBlock.classList.remove("activeContentBloc");
+    // Gestion du menu burger
+    const menuBtn = document.querySelector(".menu-btn");
+    const menuItems = document.querySelector(".menu-items");
+    const expandBtn = document.querySelectorAll(".expand-btn");
 
-    if (!otherBlock.classList.contains("activeBloc")) {
-      introBlock.classList.remove("activeIntro");
+    // Hamburger toggle
+    menuBtn.addEventListener("click", () => {
+        menuBtn.classList.toggle("open");
+        menuItems.classList.toggle("open");
+    });
+
+    // Mobile menu expand
+    expandBtn.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            btn.classList.toggle("open");
+        });
+    });
+
+    // Fonctions de gestion des blocs
+    function toggleBlock(block, contentBlock, otherBlock, otherContentBlock, introBlock) {
+        // ... (votre code de gestion des blocs)
     }
-  } else {
-    block.classList.add("activeBloc");
-    contentBlock.classList.add("activeContentBloc");
 
-    if (!otherBlock.classList.contains("activeBloc")) {
-      introBlock.classList.add("activeIntro");
+    // Fonctions d'écoute d'événement
+    function setupBlocks() {
+        const histoireBloc = document.querySelector(".histoireBloc");
+        const ressourcesBloc = document.querySelector(".ressourcesBloc");
+        const contentHistoire = document.querySelector(".contentHistoire");
+        const contentRessources = document.querySelector(".contentRessources");
+        const contentIntro = document.querySelector(".contentIntro");
+
+        histoireBloc.addEventListener("click", function () {
+            toggleBlock(histoireBloc, contentHistoire, ressourcesBloc, contentRessources, contentIntro);
+        });
+
+        ressourcesBloc.addEventListener("click", function () {
+            toggleBlock(ressourcesBloc, contentRessources, histoireBloc, contentHistoire, contentIntro);
+        });
     }
-
-    if (otherBlock.classList.contains("activeBloc")) {
-      otherBlock.classList.remove("activeBloc");
-      otherContentBlock.classList.remove("activeContentBloc");
-    }
-  }
-}
-
-// Fonctions d'écoute d'événement
-function setupBlocks() {
-  const histoireBloc = document.querySelector(".histoireBloc");
-  const ressourcesBloc = document.querySelector(".ressourcesBloc");
-  const contentHistoire = document.querySelector(".contentHistoire");
-  const contentRessources = document.querySelector(".contentRessources");
-  const contentIntro = document.querySelector(".contentIntro");
-
-  histoireBloc.addEventListener("click", function () {
-    toggleBlock(histoireBloc, contentHistoire, ressourcesBloc, contentRessources, contentIntro);
-  });
-
-  ressourcesBloc.addEventListener("click", function () {
-    toggleBlock(ressourcesBloc, contentRessources, histoireBloc, contentHistoire, contentIntro);
-  });
-}
 
 // Initialisation
 (function() {
   setupBlocks();
 })();
-
-// Gestion du menu
-(function() {
-  const menuBtn = document.querySelector(".menu-btn");
-  const menuItems = document.querySelector(".menu-items");
-  const expandBtn = document.querySelectorAll(".expand-btn");
-
-  // Hamburger toggle
-  menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("open");
-    menuItems.classList.toggle("open");
-  });
-
-  // Mobile menu expand
-  expandBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      btn.classList.toggle("open");
-    });
-  });
-})();
-
-});
-
-
 
 /* GESTION SCROLL HEADER */
 
@@ -84,11 +59,12 @@ function handleScroll() {
         document.querySelector('header').classList.remove('hidden');
     }
 
+    // Ajouter une condition pour afficher l'en-tête en haut de la page
+    if (currentScrollPosition === 0) {
+        document.querySelector('header').classList.remove('hidden');
+    }
+
     lastScrollPosition = currentScrollPosition;
 }
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    //includeHeader();
     window.addEventListener('scroll', handleScroll);
 });
